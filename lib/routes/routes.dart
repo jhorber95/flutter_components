794b60f695f02401eb8bd2960e865b965f34e64c
jhorkman bernal <jhorber95@gmail.com>
2019-09-24 17:28:16 -0500

    introduction to animated container

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets/pages/alert_page.dart';
+import 'package:widgets/pages/animated_container_page.dart';
 import 'package:widgets/pages/avatar_page.dart';
 import 'package:widgets/pages/card_page.dart';
 import 'package:widgets/pages/home_page.dart';
@@ -10,5 +11,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
     'card': (BuildContext context) => CardPage(),
+    'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
   };
 }
