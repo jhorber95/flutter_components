a59d70fdc42fac271482cfd67e1f9754826181e4
jhorkman bernal <jhorber95@gmail.com>
2019-09-24 16:51:12 -0500

    add avatar page

--- lib/pages/avatar_page.dart
+++ lib/pages/avatar_page.dart
@@ -7,6 +7,28 @@ class AvatarPage extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Component'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://i.pinimg.com/originals/b6/11/28/b6112860b60d46407771feb7e6b2957c.jpg'),
+              radius: 20.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 5.0),
+            child: CircleAvatar(
+              child: Text('JB'),
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://wallpaperbro.com/img/359588.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.account_box),
