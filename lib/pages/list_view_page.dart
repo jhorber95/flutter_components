38945fd3e4040afcc1d4a6f952c7bf988ffdbfad
jhorkman bernal <jhorber95@gmail.com>
2019-09-27 16:35:29 -0500

    add new item to list view

--- lib/pages/list_view_page.dart
+++ lib/pages/list_view_page.dart
@@ -6,7 +6,22 @@ class ListViewPage extends StatefulWidget {
 }
 
 class _ListViewPageState extends State<ListViewPage> {
-  List<int> _list = [1, 2, 3, 4, 5];
+  List<int> _list = new List();
+  int _lastItem = 0;
+
+  ScrollController _scrollController = new ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _addNewImages();
+
+    _scrollController.addListener(() {
+      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+        _addNewImages();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +34,7 @@ class _ListViewPageState extends State<ListViewPage> {
 
   Widget _createList() {
     return ListView.builder(
+        controller: _scrollController,
         itemCount: _list.length,
         itemBuilder: (BuildContext context, int index) {
           final image = _list[index];
@@ -26,7 +42,14 @@ class _ListViewPageState extends State<ListViewPage> {
             image: NetworkImage('https://picsum.photos/500/300/?image=$image'),
             placeholder: AssetImage('assets/jar-loading.gif'),
           );
-        }
-        );
+        });
+  }
+
+  void _addNewImages() {
+    for (int i = 0; i < 10; i++) {
+      _lastItem++;
+      _list.add(_lastItem);
+      setState(() {});
+    }
   }
 }
